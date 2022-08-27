15ccaaf1297a16ef13155c9ef1fac6c43601eb21
Nikolaev Viktor <maclauddd@gmail.com>
2022-08-27 23:23:35 +0300

    feat(database): added firebase service

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import { Scenes, Telegraf } from 'telegraf';
+import { getConfig } from './config/index.js';
+import { DataBaseService } from './database.js';
 
-const bot = new Telegraf<Scenes.SceneContext>(process.env.BOT_TOKEN);
+const main = async (config: AppConfig): Promise<void> => {
+  console.log(config);
+  const bot = new Telegraf<Scenes.SceneContext>(config.botToken);
+  const db = new DataBaseService(config);
 
-const main = async () => {
   bot.launch();
 
   bot.on('text', (ctx) => {
+    console.log(ctx);
+    db.writeMessage(ctx.update.message.text, ctx.update.message.date);
     ctx.reply('Hello!');
   });
 
   console.log('BOT IS STARTED 🚀');
+
+  process.once('SIGINT', () => bot.stop('SIGINT'));
+  process.once('SIGTERM', () => bot.stop('SIGTERM'));
 };
 
-main().catch((e) => console.error(e));
-
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+getConfig()
+  .then((config) => {
+    main(config);
+  })
+  .catch((e) => console.error(e));
